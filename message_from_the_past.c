770df6b95f4cbf88dc667089f303901a1ad30c1a
daysonmarshal <daysonmarshal@gmail.com>
2026-04-30 15:03:28 +0700

    Update message_from_the_past.c

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,11 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    
+    printf("\nFrom: Dayson Marshal Sitorus\n");
+    printf("Halo Dayson di masa depan!\n");
+    printf("Kalo lu lagi baca ini, berarti lu udah jauh melangkah dari titik ini.\n");
+    printf("Semoga semua usaha lu kebayar, dan lu jadi versi terbaik dari diri lu.\n");
+    printf("Oh iya, jangan lupa orang-orang yang pernah ada di perjalanan lu.\n");
+    printf("See you di puncak bro.\n");
 }
